fa3015e3ae860e07566fa828a36c512ffee0a5e1
Satyajeet-Das <dassatyajeet123@gmail.com>
2026-09-22 13:12:15 +0530

    feat: q1 and q2 solved

---
+++ test.py
@@ -0,0 +1,54 @@
+# data = [
+#     {
+#         "name1": "Sat,Sun,Mon",
+#         "name2": "Sat1,Sun1,Mon",
+#         "qty": 150
+#     },
+#     {
+#         "name1": "Tue,Wed,Thr",
+#         "name2": "Sat1,Sun,Mon1",
+#         "qty": 200
+#     },
+    
+# ]
+
+# ans = {}
+
+# for i in data:
+#     for key, value in i.items():
+#         if key == 'qty':
+#             continue
+#         names = value.split(',')
+#         for j in names:
+#             if j in ans:
+#                 ans[j] += i['qty']
+#             else:
+#                 ans[j] = i['qty']   
+
+# print(ans) 
+
+A = ['d1', 'd5', 'd6']    
+B = ['d2', 'd3', 'd4']
+
+def fuse(A, B, k):
+    scores = {}
+    for rank, v in enumerate(A):
+        scores[v] = scores.get(v, 0) + 1/(k + rank)
+    
+    for rank, v in enumerate(B):
+        scores[v] = scores.get(v, 0) + 1/(k + rank)
+        
+    temp = []
+    for key, val in scores.items():
+        temp.append([val, key])
+        
+    temp.sort();
+    temp = temp[::-1]
+    
+    ans = []
+    for _, j in temp:
+        ans.append(j)
+    
+    return ans
+
+print(fuse(A, B, 60))
